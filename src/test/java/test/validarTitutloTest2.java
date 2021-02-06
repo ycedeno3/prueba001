10d2adad87dc205c1f816128857a0cad76be79f4
yo <ycedeno363@gmail.com>
2021-02-06 16:29:55 -0300

    Agregando testng.xml

--- src/test/java/test/validarTitutloTest2.java
+++ src/test/java/test/validarTitutloTest2.java
@@ -114,15 +114,36 @@ public class validarTitutloTest2 {
         driver.findElement(By.xpath("//input[@id = 'id_email_hero_fuji']")).click();
         driver.findElement(By.xpath("//input[@id = 'id_email_hero_fuji']")).sendKeys(fakers.fakerClass.devuelveMailAleatorio());
         driver.findElement(By.xpath("//span[@class = 'cta-btn-txt']")).click();
+        Utilities.waitXseconds(3);
         driver.getCurrentUrl();
         String tituloEsperado;
         tituloEsperado = "https://www.netflix.com/signup/registration?locale=es-CL";
         String tituloActual = "";
-        tituloActual = driver.getTitle();
+        tituloActual = driver.getCurrentUrl();
         System.out.println(tituloActual);
-        Assert.assertEquals(driver.getTitle(),
+        Assert.assertEquals(driver.getCurrentUrl(),
                 tituloEsperado,
                 tituloActual);
+        driver.quit();
+    }
+
+    @Test
+    public void dataProviderEmailTest() {
+        GetProperties properties = new GetProperties();
+        String chromeDriverURL = properties.getString("CHROMEDRIVER_PATH");
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Connectis\\Documents\\Finanzas\\chromedriver_win32\\chromedriver(2).exe");
+        WebDriver driver = new ChromeDriver();
+        String baseURL = "https://www.netflix.com/";
+        driver.get(baseURL);
+        driver.manage().window().maximize();
+        System.out.println("Maximizando pantalla....!!!");
+        System.out.println("----------");
+        Utilities.waitXseconds(3);
+        driver.findElement(By.xpath("//input[@id = 'id_email_hero_fuji']")).click();
+        driver.findElement(By.xpath("//input[@id = 'id_email_hero_fuji']")).sendKeys(fakers.fakerClass.devuelveMailAleatorio());
+        driver.findElement(By.xpath("//span[@class = 'cta-btn-txt']")).click();
+        Utilities.waitXseconds(3);
         driver.quit();
     }
 }
